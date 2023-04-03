58f2a1000750c7105868c6de33e35672652e3833
Shift <shift@laravelshift.com>
2023-04-03 14:31:04 +0000

    Remove `webpack.mix.js`

--- webpack.mix.js
+++
@@ -1,8 +0,0 @@
-const mix = require('laravel-mix');
-
-mix.setPublicPath('public_html/');
-mix.ts('resources/js/app.ts', 'public_html/js').vue({version: 3})
-    .postCss('resources/css/app.css', 'public_html/css', [
-        require('tailwindcss')
-    ])
-    .sourceMaps().version();
