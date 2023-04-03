f9711f052a490b0ce9cf6f8464a8188352471b80
Shift <shift@laravelshift.com>
2023-04-03 14:27:02 +0000

    Shift cleanup

--- database/factories/SampleFactory.php
+++ database/factories/SampleFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Models\Sample;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class SampleFactory extends Factory
--- database/factories/WordFactory.php
+++ database/factories/WordFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use App\Models\User;
-use App\Models\Word;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class WordFactory extends Factory
