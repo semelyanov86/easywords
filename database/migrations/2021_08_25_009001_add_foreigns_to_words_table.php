664cb4aa6c2881487efd6f18b2213f3066f01c8f
Shift <shift@laravelshift.com>
2023-04-03 14:26:59 +0000

    Adopt anonymous migrations

--- database/migrations/2021_08_25_009001_add_foreigns_to_words_table.php
+++ database/migrations/2021_08_25_009001_add_foreigns_to_words_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddForeignsToWordsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -34,4 +34,4 @@ class AddForeignsToWordsTable extends Migration
             $table->dropForeign(['user_id']);
         });
     }
-}
+};
--- database/migrations/2021_09_04_110709_add_from_sample_column_to_words_table.php
+++ database/migrations/2021_09_04_110709_add_from_sample_column_to_words_table.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddFromSampleColumnToWordsTable extends Migration
+return new class extends Migration
 {
     public function up(): void
     {
@@ -21,4 +21,4 @@ class AddFromSampleColumnToWordsTable extends Migration
             $table->boolean('from_sample')->default(false);
         });
     }
-}
+};
